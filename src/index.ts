168f6779687db2477d70a62e2b3111ac141cb993
WilliamZeng1115 <zeng.william@gmail.com>
2023-04-15 10:17:03 -0700

    initial cookiecutter jupyter ext

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+/**
+ * Initialization data for the plotly-extension extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'plotly-extension:plugin',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension plotly-extension is activated!');
+  }
+};
+
+export default plugin;
